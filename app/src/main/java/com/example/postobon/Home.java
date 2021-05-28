53e732c057334d2c05e627a13bd4fca5f5659de2
Juan <juan.gallegomesa@gmail.com>
2021-05-28 15:05:54 -0500

    se crea menu y  traducción

--- app/src/main/java/com/example/postobon/Home.java
+++ app/src/main/java/com/example/postobon/Home.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -28,6 +33,72 @@ public class Home extends AppCompatActivity {
         listadoGrafico.setAdapter(adaptador);
 
     }
+
+
+    //ENCARGADA DE CAMBAIR LA CONFIGURACIÓN DE IDIOMA DEL TELEFONO
+    public void cambiarIdioma(String lenguaje){
+
+        Locale idioma=new Locale(lenguaje);// tipo de datos que recibe el lenguaje a configurar en el telefono
+        Locale.setDefault(idioma); //se establece el lenguaje del telefono
+
+        Configuration configuraciontelefono=getResources().getConfiguration();
+        configuraciontelefono.locale=idioma;
+        getBaseContext().getResources().updateConfiguration(configuraciontelefono,getBaseContext().getResources().getDisplayMetrics());
+
+
+    }
+
+
+
+
+    //METODO ENCARGADO DE PINTAR EL MENU DE OPCIONES
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        return true;
+
+    }
+
+    //METODO PARA CONTROLAR LA ACCIÓN (LO QUE QUIERO HACER) DE CADD ELEMENTO DEL MENU
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id=item.getItemId();
+
+        switch (id){
+
+            case(R.id.opcion1):
+
+                Intent intent1=new Intent(Home.this,Acercade.class);
+                startActivity(intent1);
+
+                break;
+
+            case(R.id.opcion2):
+
+                cambiarIdioma("en");
+                Intent intent2=new Intent(Home.this,Home.class);
+                startActivity(intent2);
+                break;
+
+            case(R.id.opcion3):
+                cambiarIdioma("es");
+                Intent intent3=new Intent(Home.this,Home.class);
+                startActivity(intent3);
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+
+
+
 
     private void crearLista(){
 
